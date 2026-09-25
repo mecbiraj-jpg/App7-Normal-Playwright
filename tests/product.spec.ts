e5b40b43393d3f1f2736d983e0828918e2d41ddf
Biraj <mec.biraj@gmail.com>
2026-09-25 17:08:50 +0530

    Update cart validation test

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -103,21 +103,4 @@ test("Add Product and Checkout", async({page})=>{
     const totalPrice = page.locator('[data-testid="order-total"]');
     await expect(totalPrice).toHaveText("$199.98");
 
-    // Remove Backpack
-    await page.locator('[data-testid="cart-item-remove-3"]').click();
-
-    await expect(
-    page.locator('[data-testid="cart-item-name-3"]')
-    ).toHaveCount(0);
-
-    // Remove Keyboard
-    await page.locator('[data-testid="cart-item-remove-7"]').click();
-
-    await expect(
-    page.locator('[data-testid="cart-item-name-7"]')
-    ).toHaveCount(0);
-
-    const emptyCart = page.locator('[data-testid="cart-empty-heading"]');
-    await expect(emptyCart).toHaveText("Your cart is empty");
-
 });
